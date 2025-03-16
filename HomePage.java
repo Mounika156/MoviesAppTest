8cac387e24cc93f6ce100154f0e87497cbafe004
Mounika156 <mounikared6@gmail.com>
2025-03-16 23:18:50 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -7,27 +8,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class HomePage {
     /*Test the heading texts of each section
 Test whether the play button is displayed or not
 Test whether the Movies are displayed, in the corresponding movies sections
 Test the Contact Us Section*/
-    By headinTexts= By.tagName("h1");
+    By headinTexts= By.xpath("//h1[contains(@class, movies)]");
+    By clkMov = By.xpath("//img[@alt='No Time to Die']");
     By playbtn = By.xpath("//button[normalize-space()='Play']");
     By listOfMovies= By.xpath("//div[@class='react-slick-item']//img");
     By contactUs = By.xpath("//p[@class='contact-us-paragraph']");
     WebDriver driver;
+    WebDriverWait wait;
 
     public HomePage(WebDriver driver){
         this.driver =driver;
+        this.wait= new WebDriverWait(driver, Duration.ofSeconds(10));
     }
-    public List<WebElement> findHeadingTexts(){
-        return driver.findElements(headinTexts);
+    public List<String> getHeadingTexts(){
+        List<String> names= new ArrayList<>();
+        for(WebElement headnames: driver.findElements(headinTexts)){
+            names.add(headnames.getText());
+        }
+
+        return names;
+    }
+
+    public void setClkMov(){
+        WebElement image= wait.until(ExpectedConditions.elementToBeClickable(clkMov));
+        image.click();
     }
     public boolean isplayBtndisp(){
-        return driver.findElement(playbtn).isDisplayed();
+
+        WebElement plybtn = wait.until(ExpectedConditions.visibilityOfElementLocated(playbtn));
+        return plybtn.isDisplayed();
     }
+
 
     public boolean isMoviesDisp(){
         List<WebElement> movies = driver.findElements(listOfMovies);
@@ -39,8 +57,6 @@ Test the Contact Us Section*/
     public  String getContactText(){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement contactElement = wait.until(ExpectedConditions.visibilityOfElementLocated(contactUs));
-
         return driver.findElement(contactUs).getText();
-
     }
 }
